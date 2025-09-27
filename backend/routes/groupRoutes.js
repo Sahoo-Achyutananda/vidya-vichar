14dba244935e056ac941eded84e834f493c69bc0
ameya-purohit <ameyajpurohit@gmail.com>
2025-09-27 16:38:06 +0530

    commit by ameya

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {createGroup, joinGroup, userGroups, getQuestion, postQuestion, updateQuestion, deleteQuestion} = require('../controllers/groupController');
+const { protect } = require('../middleware/protect'); // Import protect
+const { createGroup, joinGroup, userGroups, getQuestion, postQuestion, updateQuestion, deleteQuestion } = require('../controllers/groupController');
 
-router.post('/create', createGroup);
-router.post('/join', joinGroup);
-router.get('/', userGroups);
+// All group routes require authentication
+router.post('/create', protect, createGroup);
+router.post('/join', protect, joinGroup);
+router.get('/', protect, userGroups); // Get groups the user belongs to
 
-router.get('/:groupid/questions',getQuestion);
-router.post('/:groupid/questions',postQuestion);
-router.put('/:groupid/questions/:questionid',updateQuestion);
-router.delete('/:groupid/questions/:questionid',deleteQuestion);
+// Question routes require authentication
+router.get('/:groupid/questions', protect, getQuestion);
+router.post('/:groupid/questions', protect, postQuestion);
+router.put('/:groupid/questions/:questionid', protect, updateQuestion);
+router.delete('/:groupid/questions/:questionid', protect, deleteQuestion);
 
 module.exports = router;
-
-// add protect where ever needed
