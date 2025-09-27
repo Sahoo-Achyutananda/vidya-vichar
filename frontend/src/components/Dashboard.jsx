82c0ac512107adbf37fe9c4d8889bef206a68af7
Sahoo-Achyutananda <sahoo.official.work@gmail.com>
2025-09-27 08:53:12 +0530

    frontend pages init

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,24 @@
+
+
+function HeroSection(){
+    return (
+        <>
+            <div className="w-[50%] flex flex-col justify bg-green-800">
+                <button>Create Group</button>
+            </div>
+            <div className="w-[50%] flex flex-col justify gap-[10px]">
+                <input type="text" />
+                <button>Join Group</button>
+            </div>
+        </>
+        
+    )
+}
+
 function Dashboard() {
     return (
-        <div>
-            This is a dashboard
+        <div className="w-[100%] flex flex-row ">
+            <HeroSection/>
         </div>
     )
 }
