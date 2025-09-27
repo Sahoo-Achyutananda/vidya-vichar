010c435376a637fda7892c282e574a6e6ff08fe4
abhijithS88 <abhijithsangars@gmail.com>
2025-09-27 12:10:08 +0530

    added group routes

---
+++ backend/controllers/groupController.js
@@ -0,0 +1,29 @@
+const createGroup = async (req,res) => {
+    
+}
+
+const joinGroup = async (req,res) => {
+    
+}
+
+const userGroups = async (req,res) => {
+    
+}
+
+const getQuestion = (req,res) => {
+    
+}
+
+const postQuestion = (req,res) => {
+    
+}
+
+const updateQuestion = (req,res) => {
+    
+}
+
+const deleteQuestion = (req,res) => {
+    
+}
+
+module.exports = {createGroup, joinGroup, userGroups,getQuestion, postQuestion, updateQuestion, deleteQuestion};
---
+++ backend/routes/groupRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {createGroup, joinGroup, userGroups, getQuestion, postQuestion, updateQuestion, deleteQuestion} = require('../controllers/groupController');
+
+router.post('/create', createGroup);
+router.post('/join', joinGroup);
+router.get('/', userGroups);
+
+router.get('/:groupid/questions',getQuestion);
+router.post('/:groupid/questions',postQuestion);
+router.put('/:groupid/questions/:questionid',updateQuestion);
+router.delete('/:groupid/questions/:questionid',deleteQuestion);
+
+module.exports = router;
+
+// add protect where ever needed
