b99f5c1e7f49b61b960c73912a39b513e418c208
abhijithS88 <abhijithsangars@gmail.com>
2025-09-27 13:25:25 +0530

    small updates

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -1,5 +1,33 @@
+const Groups = require("../models/groupModel");
+
 const createGroup = async (req,res) => {
-    
+    const {groupname,username} = req.body;
+    if(!groupname || !username || username.length ===0 || groupname.length == 0 ){
+        return res.status(401).json({ message: 'both fields are required' });
+    }
+    const data = await Groups.findOne({
+        groupName : groupname
+    });
+    if(data){
+        return res.status(401).json({ message: 'group Name already exists' });
+    }
+    let accesscode;
+    while(1){
+        accesscode = (Math.floor(100000 + Math.random() * 900000)).toString();
+        const val = await Groups.findOne({accessCode:accesscode});
+        if(!val)break;
+    }
+    try{
+        const newGroup = await Groups.create({
+            groupName : groupname,
+            faculty : username,
+            accessCode : accesscode
+        });
+        return res.status(200).json({message: 'Group created', group: newGroup });
+    }catch(err){
+        console.log("mongodb insert one failed" + err.message);
+        return res.status(401).json({ message: 'server error' });
+    }
 }
 
 const joinGroup = async (req,res) => {
